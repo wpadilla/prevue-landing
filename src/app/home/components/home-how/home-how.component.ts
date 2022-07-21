6ba1144643a6c367be394d56cb51342e81c7af5f
Williams <wilfre_padilla@hotmail.com>
2022-07-21 18:58:09 -0400

    1NB: Website Build - Full Landing Page Feat/Style: home responsive Feat/Style: mission responsive Feat/Style: contact responsive

--- src/app/home/components/home-how/home-how.component.ts
+++ src/app/home/components/home-how/home-how.component.ts
@@ -10,17 +10,17 @@ export class HomeHowComponent implements OnInit {
   constructor() { }
   steps = [{
     title: `Put Yourself <br/> Out There`,
-    explain: `Start by taking a 22 second video of you; the real <br />
-      you. No filters, no bs, just tell the world who you <br />
+    explain: `Start by taking a 22 second video of you; the real
+      you. No filters, no bs, just tell the world who you
       are and what you're looking for`,
     image: 'assets/images/carousel/Carousel-Step1.png',
     numberBackground: 'assets/images/backgrounds/number-1.png',
     number: 'one',
   },
     {
-      title: `"PreVue" <br /> <span class="step-two-text">Potential Matches</span>`,
-      explain: `View our video-only feed and see what's out there!<br />
-      Choose with confidence knowing that everyone you<br />
+      title: `"PreVue" <br />Potential Matches`,
+      explain: `View our video-only feed and see what's out there!
+      Choose with confidence knowing that everyone you
       see is as authentic and unfiltered as you are.`,
       image: 'assets/images/carousel/Carousel-Step2.png',
       numberBackground: 'assets/images/backgrounds/number-2.png',
@@ -28,8 +28,8 @@ export class HomeHowComponent implements OnInit {
     },
     {
       title: `Connect!`,
-      explain: `It's a match - harness the excitement and view<br />
-      their full profile, then start a conversation via<br />
+      explain: `It's a match - harness the excitement and view
+      their full profile, then start a conversation via
       chat or video. If the vibe continues...meet up irl!`,
       image: 'assets/images/carousel/Carousel-Step3.png',
       numberBackground: 'assets/images/backgrounds/number-3.png',
